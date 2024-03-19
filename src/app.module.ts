087e3ac32b3686d6cd732dfdb9855cd2d823f3f9
Dereje Senay <dereje.467.467@gmail.com>
2024-03-19 21:45:03 +0300

    First Commit

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { RoleModule } from './role/role.module';
+import { Role } from './role/entities/role.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password',
+      database: 'dbb',
+      entities: [Role],
+      synchronize: true,
+    }),
+    RoleModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {
+  constructor(private dataSource: DataSource) {
+    console.log(dataSource.toString());
+  }
+}
